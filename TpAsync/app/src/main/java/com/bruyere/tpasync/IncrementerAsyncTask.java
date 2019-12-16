3be3523a9cfbb93b35d8a714d6d108d06d4c8ee4
remibruyere <remi.bruyere@outlook.fr>
2019-12-16 20:36:18 +0100

    Add some part of the exercice | 16-12-2019

---
+++ TpAsync/app/src/main/java/com/bruyere/tpasync/IncrementerAsyncTask.java
@@ -0,0 +1,45 @@
+package com.bruyere.tpasync;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+public class IncrementerAsyncTask extends AsyncTask<Long, Long, String> {
+
+    private Context context;
+    private TextView counter;
+
+    public IncrementerAsyncTask(Context context, TextView counter) {
+        this.context = context;
+        this.counter = counter;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        counter.setText("STARTED");
+    }
+
+    @Override
+    protected String doInBackground(Long... longs) {
+        long maxVal = longs[0];
+        for (long i = 0; i < maxVal; i++) {
+            if (i % 100000 == 0) {
+                publishProgress(i);
+            }
+        }
+        return "Finish";
+    }
+
+    @Override
+    protected void onProgressUpdate(Long... values) {
+        super.onProgressUpdate(values);
+        counter.setText(String.valueOf(values[0]));
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        counter.setText(s);
+    }
+}
--- TpAsync/app/src/main/java/com/bruyere/tpasync/MainActivity.java
+++ TpAsync/app/src/main/java/com/bruyere/tpasync/MainActivity.java
@@ -1,10 +1,11 @@
 package com.bruyere.tpasync;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,22 +14,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView counter = findViewById(R.id.counter);
+        final TextView counter = findViewById(R.id.counter);
         Button launchCounter = findViewById(R.id.launch_counter);
 
         launchCounter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                launchWithAsyncTask(counter);
             }
         });
     }
 
-    private void launchWithThread(TextView counter) {
-
+    private void launchWithThread(final TextView counter) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100_000_000; i++) {
+                    final int curr = i;
+                    if (i % 1000 == 0) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                counter.setText(String.valueOf(curr));
+                            }
+                        });
+                    }
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        counter.setText("Finish");
+                    }
+                });
+            }
+        }).start();
     }
 
     private void launchWithAsyncTask(TextView counter) {
-
+        IncrementerAsyncTask incrementerAsyncTask = new IncrementerAsyncTask(this, counter);
+        incrementerAsyncTask.execute(100_000_000L);
     }
 }
