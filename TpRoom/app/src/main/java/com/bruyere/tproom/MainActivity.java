d9af9f8fcb9fe8402ffc551c0249383014832e01
remibruyere <remi.bruyere@outlook.fr>
2020-01-08 10:38:18 +0100

    Add link adapter + viewModel

--- TpRoom/app/src/main/java/com/bruyere/tproom/MainActivity.java
+++ TpRoom/app/src/main/java/com/bruyere/tproom/MainActivity.java
@@ -1,8 +1,14 @@
 package com.bruyere.tproom;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        StudentAdapter studentAdapter = new StudentAdapter(this);
+        recyclerView.setAdapter(studentAdapter);
+
+        final StudentViewModel studentViewModel = ViewModelProviders.of(this).get(StudentViewModel.class);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Student student = new Student();
+                int i = new Random().nextInt();
+                student.setFirstName("Toto_" + i);
+                student.setLastName("TITI_" + i);
+                studentViewModel.insert(student);
+            }
+        });
     }
 }
