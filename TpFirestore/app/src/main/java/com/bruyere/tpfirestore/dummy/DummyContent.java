d89647c83d673134cc1c742a91a6a696f062758e
remibruyere <remi.bruyere@outlook.fr>
2020-01-21 18:09:18 +0100

    Fix Firestore

--- TpFirestore/app/src/main/java/com/bruyere/tpfirestore/dummy/DummyContent.java
+++ TpFirestore/app/src/main/java/com/bruyere/tpfirestore/dummy/DummyContent.java
@@ -5,13 +5,34 @@ public class DummyContent {
      * A dummy item representing a piece of content.
      */
     public static class DummyItem {
-        public final String id;
-        public final String content;
-        public final String details;
+        public String id;
+        public String content;
+        public String details;
 
-        public DummyItem(String id, String content, String details) {
+        public DummyItem() {
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
             this.id = id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
             this.content = content;
+        }
+
+        public String getDetails() {
+            return details;
+        }
+
+        public void setDetails(String details) {
             this.details = details;
         }
 
